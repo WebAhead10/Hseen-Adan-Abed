a03286eecbb3d89454e03de59fcc66186367560c
anahseen <anahseen@outlook.com>
2021-06-15 16:30:28 +0300

    hseens update

--- README.md
+++ README.md
@@ -3,18 +3,18 @@
 Quiz Master
 How well our group knows one another!
 
--- if the question is about you, you can't answer!!
+-- If the question is about you, you can't answer!!
 lets see who knows who.
 
-The quiz contains 10 questions with 4 answers with only one correct.
-you have 3 chances to answer a question correct.
+The quiz contains 10 questions with 4 answers with only one correct answer.
+You have 3 chances to answer a question correct.
 You get 4 points if answered correct on the first try.
 You get 3 points if answered correct on the second try.
 You get 2 points if answered correct on the third try.
 You get no points if you answered wrong 3 times.
 
 In this quiz we have a number of questions about our group,
-On a scale from 0-40 keep score of your knowledge of the people surrounding you.
+on a scale from 0-40 keep score of your knowledge of the people surrounding you.
 If your score is - 0-10 :: You are still strangers!
 If your score is - 11 - 20 :: You know a thing or two!
 If your score is - 21 - 35 :: You are basically friends (but not close friends)!
---
+++ script.js
@@ -0,0 +1,112 @@
+/* array of questions */
+const myQuestions = [{
+            question: "Webahead has started with kav mashve at:",
+            answers: {
+                a: "Haifa in 2016",
+                b: "Nazareth in 2017",
+                c: "Haifa in 2019",
+                d: "Nazareth in 2016 "},
+                correctAnswer: "c"
+            },
+            {
+                question: "Mahmoud and Mario have been mentoring the course together for almost:",
+                answers: {
+                    a: "one year",
+                    b: "two years",
+                    c: "three years",
+                    d: "four years"
+                },
+                correctAnswer: "b"
+            },
+            {
+                question: "Hussien is from?",
+                answers: {
+                    a: "Shefa-amer",
+                    b: "Nazareth",
+                    c: "Haifa",
+                    d: "Sakhnin"
+                },
+                correctAnswer: "c"
+            },
+            {
+                question: "Abed has finished a degree in?",
+                answers: {
+                    a: "Biology",
+                    b: "Mathmatics",
+                    c: "Law",
+                    d: "Software engineering"
+                },
+                correctAnswer: "d"
+            },
+            {
+                question: "Adan is ____ years old.",
+                answers: {
+                    a: "24",
+                    b: "23",
+                    c: "22",
+                    d: "21"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "Her/his laptop is touch and she/he is living in Jaffa :",
+                answers: {
+                    a: "Nidaa",
+                    b: "Elias",
+                    c: "Hasan",
+                    d: "Fadi"
+                },
+                correctAnswer: "c"
+            },
+            {
+                question: "She/he has a degree in Mathmatics and has two little children?",
+                answers: {
+                    a: "Dana",
+                    b: "Shireen",
+                    c: "Nidaa",
+                    d: "Adan"
+                },
+                correctAnswer: "c"
+            },
+            {
+                question: "Elias is from?",
+                answers: {
+                    a: "Jdaide Maker",
+                    b: "Haifaa",
+                    c: "Jaffa",
+                    d: "Jerusalem"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "Fadi is ____ years old.",
+                answers: {
+                    a: "25",
+                    b: "26",
+                    c: "27",
+                    d: "28"
+                },
+                correctAnswer: "d"
+            },
+            {
+                question: "Nour is from?",
+                answers: {
+                    a: "Bqeaa",
+                    b: "Sakhnin",
+                    c: "Um elfahem",
+                    d: "Jatt"
+                },
+                correctAnswer: "b"
+            }
+        ];
+
+        /** global variables */
+        let count = 0;
+        let numOfTries = 0;
+        let isCorrect = false;
+
+        let theQuestions = document.getElementById('questions'); // parent
+        function startQuiz(){
+            
+        } 
+        
